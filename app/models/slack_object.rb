dd5c9c9a9be1c6ae7548879bce5a77917a645aca
Josh Woodward <jdw6359@rit.edu>
2015-08-31 17:06:23 -0400

    removed old slack object files, added effort log migration

--- app/models/slack_object.rb
+++
@@ -1,2 +0,0 @@
-class SlackObject < ActiveRecord::Base
-end
---
+++ db/migrate/20150831205404_create_effort_log.rb
@@ -0,0 +1,13 @@
+class CreateEffortLog < ActiveRecord::Migration
+  def change
+    create_table :effort_logs do |t|
+    	t.string :name, null: false 
+    	t.string :real_name, null: false
+    	t.datetime :start_time, null: false
+    	t.datetime :end_time, null: false
+    	t.string :comment, null: false
+    	
+    	t.timestamps, null: false
+    end
+  end
+end
