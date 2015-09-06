de8f709db4c2cee383c40af9317d3b42a07c0128
Kocsen <kocsenc@gmail.com>
2015-09-06 12:56:27 -0400

    - Now showing start time - added sampel data for development - added time zone support

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+case Rails.env
+  # Call this by running `rake db:seed RAILS_ENV=development-sample`
+  when "development-sample"
+    EffortLog.create(name: 'test', real_name: 'potato skins', url: 'http://google.com',
+                     end_time: DateTime.now, start_time: DateTime.now - 5.hours, hours: 5, comment: 'Put chese and baked potatos')
+  when "production"
+    # Nothing yet, not used
+  else
+    # Else nothing
+end
